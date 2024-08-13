fcfca21ea80879784c25add85c03a31739c05018
zhuy456z <huylahoai000@gmail.com>
2024-08-13 11:47:54 +0700

    Add player model,camera follow,player controller function ,Create title map

---
+++ Assets/Scripts/CameraMotor.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMotor : MonoBehaviour
+{
+    [SerializeField]  Transform lookAt;
+    [SerializeField] float boundX = 0.15f;
+    [SerializeField] float boundY = 0.05f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 delta = Vector3.zero;
+        //check if inside the bounds on the x axis
+        float deltaX = lookAt.position.x - transform.position.x;
+        if(deltaX > boundX || deltaX < -boundX)
+        {
+            if(transform.position.x < lookAt.position.x)
+            {
+                delta.x = deltaX - boundX;
+            }
+            else
+            {
+                delta.x = deltaX + boundX;
+            }
+        }
+        //check if inside the bounds on the y axis
+        float deltaY = lookAt.position.y - transform.position.y;
+        if (deltaY > boundY || deltaY < -boundY)
+        {
+            if (transform.position.y < lookAt.position.y)
+            {
+                delta.y = deltaY - boundY;
+            }
+            else
+            {
+                delta.y = deltaY + boundY;
+            }
+        }
+        transform.position += new Vector3(delta.x, delta.y, 0);
+    }
+}
